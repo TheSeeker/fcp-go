c3f4ea6ed684ae81270e1f8c7ded01a89e96a02f
The Seeker <TheSeeker+github@gmail.com>
2020-05-22 21:00:11 -0700

    Broken, thinking about how to start over.

--- ClientGet.go
+++ ClientGet.go
@@ -1,7 +1,6 @@
 package fcp
 
 import (
-	//	"fmt"
 	"strconv"
 )
 
--- fcp.go
+++ fcp.go
@@ -16,12 +16,13 @@ type FCPClient struct {
 	socket     *net.TCPConn
 	msgSender  chan message
 	msgHandler chan message
+	caller     chan clientMessage
 }
 
 type message struct {
-	message string
-	params  []string
-	data    *[]byte
+	name   string
+	params []string
+	data   *[]byte
 }
 
 func NewClient(ipPort string, ssl bool, id string) (FCPClient, error) {
@@ -39,9 +40,9 @@ func (r *FCPClient) sender() {
 			fmt.Println(err.Error())
 			return
 		}
-		r.socket.Write([]byte(msg.message))
+		r.socket.Write([]byte(msg.name))
 		r.socket.Write([]byte("\n"))
-		fmt.Println(msg.message) //crappy debug output is crappy
+		fmt.Println(msg.name) //crappy debug output is crappy
 		for i := range msg.params {
 			r.socket.Write([]byte(msg.params[i]))
 			r.socket.Write([]byte("\n"))
@@ -58,72 +59,130 @@ func (r *FCPClient) sender() {
 	}
 }
 
-func (r *FCPClient) handler() {
+func (r *FCPClient) handler(caller chan message) {
 	for {
 		msg := <-r.msgHandler
 
-		switch msg.message {
+		switch msg.name {
 		case "NodeHello":
+			caller <- nodeHello.parseMessage(params)
 		case "CloseConnectionDuplicateClientName":
+			fmt.Println("Unimplemented")
 		case "Peer":
+			fmt.Println("Unimplemented")
 		case "PeerNote":
+			fmt.Println("Unimplemented")
 		case "EndListPeers":
+			fmt.Println("Unimplemented")
 		case "EndListPeerNotes":
+			fmt.Println("Unimplemented")
 		case "PeerRemoved":
+			fmt.Println("Unimplemented")
 		case "NodeData":
+			fmt.Println("Unimplemented")
 		case "ConfigData": // (since 1027)
+			fmt.Println("Unimplemented")
 		case "TestDDAReply": // (since 1027)
+			fmt.Println("Unimplemented")
 		case "TestDDAComplete": // (since 1027)
+			fmt.Println("Unimplemented")
 		case "SSKKeyPair":
+			fmt.Println("Unimplemented")
 		case "PersistentGet":
+			fmt.Println("Unimplemented")
 		case "PersistentPut":
+			fmt.Println("Unimplemented")
 		case "PersistentPutDir":
+			fmt.Println("Unimplemented")
 		case "URIGenerated":
+			fmt.Println("Unimplemented")
 		case "PutSuccessful":
+			fmt.Println("Unimplemented")
 		case "PutFetchable":
+			fmt.Println("Unimplemented")
 		case "DataFound":
+			fmt.Println("Unimplemented")
 		case "GetRequestStatus":
+			fmt.Println("Unimplemented")
 		case "AllData":
+			fmt.Println("Unimplemented")
 		case "StartedCompression":
+			fmt.Println("Unimplemented")
 		case "FinishedCompression":
+			fmt.Println("Unimplemented")
 		case "SimpleProgress":
+			fmt.Println("Unimplemented")
 		case "ExpectedHashes": // (since 1254)
+			fmt.Println("Unimplemented")
 		case "ExpectedMIME": // (since 1307)
+			fmt.Println("Unimplemented")
 		case "ExpectedDataLength": // (since 1307)
+			fmt.Println("Unimplemented")
 		case "CompatibilityMode": // (since 1254)
+			fmt.Println("Unimplemented")
 		case "EndListPersistentRequests":
+			fmt.Println("Unimplemented")
 		case "PersistentRequestRemoved": // (since 1016)
+			fmt.Println("Unimplemented")
 		case "PersistentRequestModified": // (since 1016)
+			fmt.Println("Unimplemented")
 		case "SendingToNetwork": // (since 1207)
+			fmt.Println("Unimplemented")
 		case "EnterFiniteCooldown": // (since 1365)
+			fmt.Println("Unimplemented")
 		case "GeneratedMetadata": // (since 1380)
+			fmt.Println("Unimplemented")
 
 		case "PutFailed":
+			fmt.Println("Unimplemented")
 		case "GetFailed":
+			fmt.Println("Unimplemented")
 		case "ProtocolError":
+			fmt.Println("Unimplemented")
 		case "IdentifierCollision":
+			fmt.Println("Unimplemented")
 		case "UnknownNodeIdentifier":
+			fmt.Println("Unimplemented")
 		case "UnknownPeerNoteType":
+			fmt.Println("Unimplemented")
 		case "SubscribedUSK":
+			fmt.Println("Unimplemented")
 		case "SubscribedUSKUpdate":
+			fmt.Println("Unimplemented")
 		case "SubscribedUSKSendingToNetwork": //(since 1365)
+			fmt.Println("Unimplemented")
 		case "SubscribedUSKRoundFinished": // (since 1365)
+			fmt.Println("Unimplemented")
 
 		case "PluginInfo": // (since 1075)
+			fmt.Println("Unimplemented")
 		case "PluginRemoved": // (since 1227)
+			fmt.Println("Unimplemented")
 		case "FCPPluginReply": // (since 1075)
+			fmt.Println("Unimplemented")
 
 		case "ProbeBandwidth":
+			fmt.Println("Unimplemented")
 		case "ProbeBuild":
+			fmt.Println("Unimplemented")
 		case "ProbeError":
+			fmt.Println("Unimplemented")
 		case "ProbeIdentifier":
+			fmt.Println("Unimplemented")
 		case "ProbeLinkLengths":
+			fmt.Println("Unimplemented")
 		case "ProbeLocation":
+			fmt.Println("Unimplemented")
 		case "ProbeRefused":
+			fmt.Println("Unimplemented")
 		case "ProbeRejectStats":
+			fmt.Println("Unimplemented")
 		case "ProbeStoreSize":
+			fmt.Println("Unimplemented")
 		case "ProbeUptime":
+			fmt.Println("Unimplemented")
 		default:
+			fmt.Println("Unknown Node Message " + msg.name + " | " + msg.params)
 			//Unknown Message
 		}
 	}
